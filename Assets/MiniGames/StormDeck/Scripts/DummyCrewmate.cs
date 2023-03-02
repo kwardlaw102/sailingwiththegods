5799f7e3ba91b0fe914f26f1ded6be72053bd523
Kenny Wardlaw <kwardlaw102@gmail.com>
2023-03-02 15:05:39 -0500

    Added nameplates showing crewmembers' names and types

--- Assets/MiniGames/StormDeck/Scripts/DummyCrewmate.cs
+++ Assets/MiniGames/StormDeck/Scripts/DummyCrewmate.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DummyCrewmate : MonoBehaviour
 {
 	public string crewmateName = "Crewmate";
+	public string crewmateType = "Sailor";
+	[TextArea(1,10)]
+	public string nameplateFormat = "{0}\n-{1}-";
+	public TextMeshProUGUI nameplate;
+
+	private void Start() {
+		UpdateNameplate();
+	}
 
 	public void Say(string message) {
 		Debug.Log(crewmateName + " says, \"" + message + "\"");
 	}
+
+	private void UpdateNameplate() {
+		nameplate.text = string.Format(nameplateFormat, crewmateName, crewmateType);
+	}
+
 }
